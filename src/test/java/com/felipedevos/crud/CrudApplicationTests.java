311c1c4026d19e8d1325aba087caf3b15d64f5ce
fdevos1 <felipe.devosm@gmail.com>
2023-09-27 14:56:18 -0300

    project created

---
+++ src/test/java/com/felipedevos/crud/CrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.felipedevos.crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
