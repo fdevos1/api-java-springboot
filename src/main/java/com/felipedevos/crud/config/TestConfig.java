4951fd2bf6f10cb41b54f035107b0411b5470ffd
fdevos1 <felipe.devosm@gmail.com>
2023-10-06 12:08:36 -0300

    category

--- src/main/java/com/felipedevos/crud/config/TestConfig.java
+++ src/main/java/com/felipedevos/crud/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.felipedevos.crud.entities.Category;
 import com.felipedevos.crud.entities.Order;
 import com.felipedevos.crud.entities.User;
 import com.felipedevos.crud.entities.enums.OrderStatus;
+import com.felipedevos.crud.repositories.CategoryRepository;
 import com.felipedevos.crud.repositories.OrderRepository;
 import com.felipedevos.crud.repositories.UserRepository;
 
@@ -23,16 +25,24 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
 		User u1 = new User(null, "Maria Brown", "maria@fakemail.com", "987654321", "123456");
 		User u2 = new User(null, "Alex Green", "alex@fakemail.com", "987654321", "123456");
+		
 		Order o1 = new Order(null, Instant.parse("2023-09-05T21:00:00Z"), OrderStatus.WAITING_PAYMENT, u1);
 		Order o2 = new Order(null, Instant.parse("2023-09-05T21:30:00Z"), OrderStatus.WAITING_PAYMENT, u2);
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
 
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2));
+		categoryRepository.saveAll(Arrays.asList(cat1,cat2));
 	}
 
 }
---
+++ src/main/java/com/felipedevos/crud/services/CategoryService.java
@@ -0,0 +1,27 @@
+package com.felipedevos.crud.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.felipedevos.crud.entities.Category;
+import com.felipedevos.crud.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll() {
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		
+		return obj.get();
+	}
+}
