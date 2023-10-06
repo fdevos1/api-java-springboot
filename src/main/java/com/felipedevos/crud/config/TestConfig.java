079103bb4b51503d3b6277a90dedbc6b7d9ce1cb
fdevos1 <felipe.devosm@gmail.com>
2023-10-05 21:55:31 -0300

    order enum

--- src/main/java/com/felipedevos/crud/config/TestConfig.java
+++ src/main/java/com/felipedevos/crud/config/TestConfig.java
@@ -10,6 +10,7 @@ import org.springframework.context.annotation.Profile;
 
 import com.felipedevos.crud.entities.Order;
 import com.felipedevos.crud.entities.User;
+import com.felipedevos.crud.entities.enums.OrderStatus;
 import com.felipedevos.crud.repositories.OrderRepository;
 import com.felipedevos.crud.repositories.UserRepository;
 
@@ -19,7 +20,7 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private UserRepository userRepository;
-	
+
 	@Autowired
 	private OrderRepository orderRepository;
 
@@ -27,11 +28,11 @@ public class TestConfig implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		User u1 = new User(null, "Maria Brown", "maria@fakemail.com", "987654321", "123456");
 		User u2 = new User(null, "Alex Green", "alex@fakemail.com", "987654321", "123456");
-		Order o1 = new Order(null, Instant.parse("2023-09-05T21:00:00Z"), u1);
-		Order o2 = new Order(null, Instant.parse("2023-09-05T21:30:00Z"), u2);
-		
-		userRepository.saveAll(Arrays.asList(u1,u2));
-		orderRepository.saveAll(Arrays.asList(o1,o2));
+		Order o1 = new Order(null, Instant.parse("2023-09-05T21:00:00Z"), OrderStatus.WAITING_PAYMENT, u1);
+		Order o2 = new Order(null, Instant.parse("2023-09-05T21:30:00Z"), OrderStatus.WAITING_PAYMENT, u2);
+
+		userRepository.saveAll(Arrays.asList(u1, u2));
+		orderRepository.saveAll(Arrays.asList(o1, o2));
 	}
 
 }
--- src/main/java/com/felipedevos/crud/entities/Order.java
+++ src/main/java/com/felipedevos/crud/entities/Order.java
@@ -5,6 +5,7 @@ import java.time.Instant;
 import java.util.Objects;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.felipedevos.crud.entities.enums.OrderStatus;
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -18,13 +19,15 @@ import jakarta.persistence.Table;
 @Table(name = "tb_order")
 public class Order implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
 	@JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
 	private Instant moment;
+
+	private Integer orderStatus;
 
 	@ManyToOne
 	@JoinColumn(name = "client_id")
@@ -34,10 +37,11 @@ public class Order implements Serializable {
 
 	}
 
-	public Order(Long id, Instant moment, User client) {
+	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
 		this.moment = moment;
+		setOrderStatus(orderStatus);
 		this.client = client;
 	}
 
@@ -63,6 +67,17 @@ public class Order implements Serializable {
 
 	public void setClient(User client) {
 		this.client = client;
+	}
+
+	public OrderStatus getOrderStatus() {
+		return OrderStatus.valueOf(orderStatus);
+	}
+
+	public void setOrderStatus(OrderStatus orderStatus) {
+		if (orderStatus != null) {
+
+			this.orderStatus = orderStatus.getCode();
+		}
 	}
 
 	@Override
