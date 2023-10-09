730c773235e479de3d61443577baa8f93e8c4b91
fdevos1 <felipe.devosm@gmail.com>
2023-10-09 14:45:22 -0300

    order item

--- src/main/java/com/felipedevos/crud/config/TestConfig.java
+++ src/main/java/com/felipedevos/crud/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.felipedevos.crud.entities.Category;
 import com.felipedevos.crud.entities.Order;
+import com.felipedevos.crud.entities.OrderItem;
 import com.felipedevos.crud.entities.Product;
 import com.felipedevos.crud.entities.User;
 import com.felipedevos.crud.entities.enums.OrderStatus;
 import com.felipedevos.crud.repositories.CategoryRepository;
+import com.felipedevos.crud.repositories.OrderItemRepository;
 import com.felipedevos.crud.repositories.OrderRepository;
 import com.felipedevos.crud.repositories.ProductRepository;
 import com.felipedevos.crud.repositories.UserRepository;
@@ -29,10 +31,14 @@ public class TestConfig implements CommandLineRunner {
 	private OrderRepository orderRepository;
 	
 	@Autowired
+	private OrderItemRepository orderItemRepository;
+
+	@Autowired
 	private CategoryRepository categoryRepository;
 	
 	@Autowired
 	private ProductRepository productRepository;
+
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -48,10 +54,14 @@ public class TestConfig implements CommandLineRunner {
 		
 		Product p1 = new Product(null, "The Lord of the Rings", "Lorem ipsum", 90.5, "");
 		Product p2 = new Product(null, "Macbook", "Lorem ipsum", 1250.0, "");
+		
+		OrderItem oi1 = new OrderItem(o1,p1,2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1,p2,1 , p2.getPrice());
+		OrderItem oi3 = new OrderItem(o2,p2,1 , p2.getPrice());
 
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2));
-		categoryRepository.saveAll(Arrays.asList(cat1,cat2, cat3));
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 		productRepository.saveAll(Arrays.asList(p1,p2));
 		
 		p1.getCategories().add(cat2);
@@ -59,6 +69,7 @@ public class TestConfig implements CommandLineRunner {
 		p2.getCategories().add(cat3);
 		
 		productRepository.saveAll(Arrays.asList(p1,p2));
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3));
 	}
 
 }
--- src/main/java/com/felipedevos/crud/entities/Order.java
+++ src/main/java/com/felipedevos/crud/entities/Order.java
@@ -2,7 +2,9 @@ package com.felipedevos.crud.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.felipedevos.crud.entities.enums.OrderStatus;
@@ -13,6 +15,7 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 
 @Entity
@@ -32,6 +35,9 @@ public class Order implements Serializable {
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
+
+	@OneToMany(mappedBy = "id.order")
+	private Set<OrderItem> items = new HashSet<>();
 
 	public Order() {
 
@@ -78,6 +84,10 @@ public class Order implements Serializable {
 
 			this.orderStatus = orderStatus.getCode();
 		}
+	}
+
+	public Set<OrderItem> getItems() {
+		return items;
 	}
 
 	@Override
